d330ac229bad05fc7dcac4c77aba70c93aa1598c
Robert <robert@Davids-MacBook-Pro.local>
2014-12-20 17:09:35 -0500

    added views

--- utils/mrd/appmain.py
+++ utils/mrd/appmain.py
@@ -1,8 +1,9 @@
-from app import app
+__author__ = 'robert'
+
 from flask import Flask, session, redirect, url_for, escape, request
-from utils.mrd.auth import login_manager
 
 app = Flask(__name__)
+login_manager = LoginManager()
 login_manager.init_app(app)
 
 @app.route('/')
@@ -18,10 +19,21 @@ def login():
         flash("Logged in successfully.")
         return redirect(request.args.get("next") or url_for("index"))
     return render_template("login.html", form=form)
+    '''
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
     return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = b'\xff\x02\xf5;\xe9\x00+;\xc9\xde\x08Re\xfe\x90\xb8{\x1a!\x0e\xe6EH\xee'
+
+if (__name__) == "__main__":
+    app.run()
+
+
+
+
 
