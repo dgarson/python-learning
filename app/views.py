5d80559a2a789699aec677ca5649cb2c9eda9f20
Robert <robert@Davids-MacBook-Pro.local>
2014-12-20 21:35:41 -0500

    removed redundant routes

--- app/views.py
+++ app/views.py
@@ -1,12 +1,22 @@
-__author__ = 'robert'
-
-from flask import flash, redirect, render_template, request, url_for
-from .main import app
-from .models import Account
-from .database import db
-from .forms import LoginForm, RegistrationForm
-from flask_login import login_user, login_required, logout_user
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from app import app
+from app.models import Account
+from app.database import db, db_session
+from app.forms import LoginForm, RegistrationForm
+
+__author__ = 'dgarson'
+
+login_manager = LoginManager()
+# login_manager.login_view = 'users.login'
+login_manager.init_app(app)
+
+users = Blueprint('users', __name__)
+
+@login_manager.user_loader
+def load_user(userid):
+    return Account.query.get(userid)
+
 
 @app.route('/')
 def index():
